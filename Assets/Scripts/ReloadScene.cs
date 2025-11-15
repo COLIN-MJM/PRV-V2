51c085e0fe6d05b73df229f272a817551aac6c1c
BartProjectCode <bisliwa@myges.fr>
2025-11-15 14:16:28 +0100

    mini update R to reload scene 💯

---
+++ Assets/Scripts/ReloadScene.cs
@@ -0,0 +1,14 @@
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ReloadScene : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+}
