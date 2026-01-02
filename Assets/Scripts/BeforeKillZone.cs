aa5f4eba69233269e7d32c3df4cb2aa834bb39f6
BartProjectCode <bisliwa@myges.fr>
2026-01-02 15:47:13 +0100

    Killzone ajouté
    8 sur le numpad, quand tu cliques sur le terrain, une killzone apparaît après x temps (sur le timer de BeforeKillZone)

---
+++ Assets/Scripts/BeforeKillZone.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class BeforeKillZone : MonoBehaviour
+{
+    public float time;
+    public GameObject killZone;
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        time -= Time.deltaTime;
+
+        if (time <= 0)
+        {
+            //logique
+
+            Instantiate(killZone, transform.position, transform.rotation);
+            
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/TimerBeforeDestroy.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class TimerBeforeDestroy : MonoBehaviour
+{
+    public float timer;
+    void Update()
+    {
+        timer -= Time.deltaTime;
+
+        if (timer <= 0)
+        {
+            Destroy(gameObject);
+        }
+        
+    }
+}
