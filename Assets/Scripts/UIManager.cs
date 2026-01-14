5db40d6b95b2b14b72a4adbada7b7dd44ccb3dc6
COLIN-MJM <matthieu.j.m.colin@gmail.com>
2026-01-14 16:54:35 +0100

    Choix du cliquable via molette

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -4,8 +4,17 @@ using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
+    public GameObject gm;
+    public InputReader inputReader;
+    
     public List<GameObject> buttons;
     private int lastlyClickedButton = 0;
+
+    private void Start()
+    {
+        gm = GameObject.FindGameObjectWithTag("GM");
+        inputReader = gm.GetComponent<InputReader>();
+    }
 
     private void Update()
     {
@@ -13,12 +22,30 @@ public class UIManager : MonoBehaviour
         {
             if (buttons[i].GetComponent<UIActions>().isSelected)
             {
-                if (lastlyClickedButton != i)
+                if (lastlyClickedButton == i)
+                {
+                    ChangeSelectionByWheel(i);
+                }
+                else
                 {
                     buttons[lastlyClickedButton].GetComponent<UIActions>().isSelected = false;
                     lastlyClickedButton = i;
                 }
             }
         }
+    }
+
+    private void ChangeSelectionByWheel(int i)
+    {
+        int newSelect = i + inputReader.Mousewheel;
+        if (newSelect > buttons.Count - 1)
+        {
+            newSelect = 0;
+        }
+        else if (newSelect < 0)
+        {
+            newSelect = buttons.Count - 1;
+        }
+        buttons[newSelect].GetComponent<UIActions>().isSelected = true;
     }
 }
