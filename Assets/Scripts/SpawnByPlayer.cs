aa2615d1b41305c9178bf83af7096aabd237a792
BartProjectCode <bisliwa@myges.fr>
2026-01-15 18:08:40 +0100

    Compte et cooldown pour les outils, visible dans l'inspecteur de l'objet ground

--- Assets/Scripts/SpawnByPlayer.cs
+++ Assets/Scripts/SpawnByPlayer.cs
@@ -18,6 +18,28 @@ public class SpawnByPlayer : MonoBehaviour, IPointerClickHandler
     
     private Text text;
 
+    [Header("Outils disponible, max dispo et timer")]
+    public int foodCount;
+    public int maxFoodCount = 5;
+    public float foodTimer;
+    public float maxFoodTimer = 5f;
+    
+    public float zoneCount;
+    public float maxZoneCount = 5;
+    public float zoneTimer;
+    public float maxZoneTimer = 5f;
+
+    public int maxEggCount = 3;
+    
+    public int eggOneCount;
+    
+    public int eggTwoCount;
+    
+    public int eggThreeCount;
+    
+    public int eggSixCount;
+
+
     void Awake()
     {
         // Load the Arial font from the Unity Resources folder.
@@ -59,6 +81,8 @@ public class SpawnByPlayer : MonoBehaviour, IPointerClickHandler
     {
         gm = GameObject.FindGameObjectWithTag("GM");
         inputReader = gm.GetComponent<InputReader>();
+
+        foodCount = maxFoodCount;
         
         currentChoice = choices[0];
         text.text = $"Current Spawn if Right Click: {currentChoice.name}";
@@ -68,6 +92,9 @@ public class SpawnByPlayer : MonoBehaviour, IPointerClickHandler
     {
         currentChoice = choices[choiceIndex];
         text.text = $"Current Spawn if Right Click: {currentChoice.name}";
+        
+        OnTimerGetFood();
+        OnTimerGetKillZone();
     }
 
     public void OnPointerClick(PointerEventData eventData)
@@ -75,7 +102,82 @@ public class SpawnByPlayer : MonoBehaviour, IPointerClickHandler
         if (eventData.button == PointerEventData.InputButton.Left)
         {
             // Instantiate(currentChoice, new Vector3(eventData.pointerPressRaycast.worldPosition.x, 0f, eventData.pointerPressRaycast.worldPosition.z), Quaternion.identity);
-            Instantiate(currentChoice, eventData.pointerPressRaycast.worldPosition, Quaternion.identity);
+            if (currentChoice.name == "Food" && foodCount > 0)
+            {
+                foodCount--;
+                Instantiate(currentChoice, eventData.pointerPressRaycast.worldPosition, Quaternion.identity);
+            }
+            else if (currentChoice.name == "Alcool" && zoneCount > 0)
+            {
+                zoneCount--;
+                Instantiate(currentChoice, eventData.pointerPressRaycast.worldPosition, Quaternion.identity);
+            }
+            else if (currentChoice.name == "Eggs species1" && eggOneCount > 0)
+            {
+                eggOneCount--;
+                Instantiate(currentChoice, eventData.pointerPressRaycast.worldPosition, Quaternion.identity);
+            }
+            else if (currentChoice.name == "Eggs species2" && eggTwoCount > 0)
+            {
+                eggTwoCount--;
+                Instantiate(currentChoice, eventData.pointerPressRaycast.worldPosition, Quaternion.identity);
+            }
+            else if (currentChoice.name == "Eggs species3" && eggThreeCount > 0)
+            {
+                eggThreeCount--;
+                Instantiate(currentChoice, eventData.pointerPressRaycast.worldPosition, Quaternion.identity);
+            }
+            else if (currentChoice.name == "Eggs species6" && eggSixCount > 0)
+            {
+                eggSixCount--;
+                Instantiate(currentChoice, eventData.pointerPressRaycast.worldPosition, Quaternion.identity);
+            }
+            else if (currentChoice.name == "Scarecrow")
+            {
+                Instantiate(currentChoice, eventData.pointerPressRaycast.worldPosition, Quaternion.identity);
+            }
         }
     }
+
+    public void OnTimerGetFood()
+    {
+        if (foodCount < maxFoodCount)
+        {
+            foodTimer += Time.deltaTime;
+        }
+
+        if (foodTimer >= maxFoodTimer)
+        {
+            foodTimer = 0;
+            foodCount++;
+        }
+
+        if (foodCount >= maxFoodCount)
+        {
+            foodCount = maxFoodCount;
+            foodTimer = 0;
+        }
+        
+    }
+    
+    public void OnTimerGetKillZone()
+    {
+        if (zoneCount < maxZoneCount)
+        {
+            zoneTimer += Time.deltaTime;
+        }
+
+        if (zoneTimer >= maxZoneTimer)
+        {
+            zoneTimer = 0;
+            zoneCount++;
+        }
+
+        if (zoneCount >= maxZoneCount)
+        {
+            zoneCount = maxZoneCount;
+            zoneTimer = 0;
+        }
+        
+    }   
 }
