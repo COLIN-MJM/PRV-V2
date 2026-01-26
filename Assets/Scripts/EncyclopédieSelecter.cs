a0908139c482c074a29e7bfba8dcc44342d6896d
Ethan <elancelle@myges.fr>
2026-01-26 20:21:38 +0100

    UI-Fixed

--- Assets/Scripts/EncyclopédieSelecter.cs
+++ Assets/Scripts/EncyclopédieSelecter.cs
@@ -11,18 +11,53 @@ public class EncyclopedieSelecter : MonoBehaviour
     public bool isOpened = false;
     private VCA masterVCA;
     
+    //OUTLINE
+    private Transform fbx;
+    private Material[] childMatList;
+    private Material secondMat;
+    private Color selfColor;
+    
     void Start()
     {
         cam = Camera.main;
         collider = GetComponent<BoxCollider>();
         eventEmitter = GetComponent<StudioEventEmitter>();
         masterVCA = RuntimeManager.GetVCA("VCA:/Master");
+        
+        //OUTLINE
+        fbx = transform.GetChild(0);
+        childMatList = new Material[fbx.childCount];
+        for (int i = 0; i < fbx.childCount; i++)
+        {
+            Transform actualChild = fbx.transform.GetChild(i);
+            childMatList[i] = actualChild.GetComponent<MeshRenderer>().materials[1];
+        }
+        selfColor = childMatList[0].GetColor("_Outline_Color");
     }
 
     // Update is called once per frame
     void Update()
     {
         eventEmitter.SetParameter("EncyclopedieSounds", 0);
+
+        
+        //OUTLINE
+        if (VerifyHovering())
+        {
+            for (int i = 0; i < childMatList.Length; i++)
+            {
+                secondMat = childMatList[i];
+                secondMat.SetFloat("_Outline_Opacity", 1f);
+            }
+        }
+        else
+        {
+            for (int i = 0; i < childMatList.Length; i++)
+            {
+                secondMat = childMatList[i];
+                secondMat.SetFloat("_Outline_Opacity", 0f);
+            }
+        }
         
         if (VerifyClicking())
         {
