16aaedc3cd50dc21fc588006ca67c82e90778aa7
COLIN-MJM <matthieu.j.m.colin@gmail.com>
2026-01-26 20:57:17 +0100

    surprise!

--- Assets/Scripts/EntityCount.cs
+++ Assets/Scripts/EntityCount.cs
@@ -1,16 +1,30 @@
+using System;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EntityCount : MonoBehaviour
 {
     public int entityCount;
     
     public int vanilliCount;
+    public int dyingVanilliCount;
     public int enXCount;
+    public int dyingEnXCount;
     public int holoCount;
+    public int dyingHoloCount;
     public int toxiCount;
-
-
+    public int dyingToxiCount;
     
+    [SerializeField] private TMP_Text vanilliCountText;
+    [SerializeField] private TMP_Text vanilliDeathText;
+    [SerializeField] private TMP_Text enXCountText;
+    [SerializeField] private TMP_Text enXDeathText;
+    [SerializeField] private TMP_Text holoCountText;
+    [SerializeField] private TMP_Text holoDeathText;
+    [SerializeField] private TMP_Text toxiCountText;
+    [SerializeField] private TMP_Text toxiDeathText;
+
     // LES +1 ON START SONT FAIT DANS LE SCRIPT STATECHECKER
     // LES -1 SONT FAIT DANS LES SCRIPTS APPROPRIE POUR CHAQUE MORT POSSIBLE, PAR EXEMPLE : LE SCRIPT INTERACT
     
@@ -76,18 +90,46 @@ public class EntityCount : MonoBehaviour
         if (objectEntityID.species == Species.S1)
         {
             VanilliCountMod(value);
+            vanilliCountText.text = vanilliCount.ToString();
         }
         else if (objectEntityID.species == Species.S2)
         {
             EnXCountMod(value);
+            enXCountText.text = enXCount.ToString();
         } 
         else if (objectEntityID.species == Species.S3)
         {
             HoloCountMod(value);
+            holoCountText.text = holoCount.ToString();
         }
         else if (objectEntityID.species == Species.S6)
         {
             ToxiCountMod(value);
+            toxiCountText.text = toxiCount.ToString();
+        }
+
+        if (value < 0)
+        {
+            if (objectEntityID.species == Species.S1)
+            {
+                dyingVanilliCount -= value;
+                vanilliDeathText.text = dyingVanilliCount.ToString();
+            }
+            else if (objectEntityID.species == Species.S2)
+            {
+                dyingEnXCount -= value;
+                enXDeathText.text = dyingEnXCount.ToString();
+            } 
+            else if (objectEntityID.species == Species.S3)
+            {
+                dyingHoloCount -= value;
+                holoDeathText.text = dyingHoloCount.ToString();
+            }
+            else if (objectEntityID.species == Species.S6)
+            {
+                dyingToxiCount -= value;
+                toxiDeathText.text = dyingToxiCount.ToString();
+            }
         }
     }
 
