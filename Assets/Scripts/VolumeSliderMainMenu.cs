c6e13b709c9dfdd866b90ceb47aac6d0a30ca714
COLIN-MJM <matthieu.j.m.colin@gmail.com>
2026-01-27 00:16:41 +0100

    fix slider main menu

--- Assets/Scripts/VolumeSliderMainMenu.cs
+++ Assets/Scripts/VolumeSliderMainMenu.cs
@@ -2,14 +2,23 @@ using System;
 using FMOD.Studio;
 using FMODUnity;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class VolumeSliderMainMenu : MonoBehaviour
 {
     private VCA masterVCA;
+    [SerializeField] private Slider volumeSlider;
 
     private void Start()
     {
         masterVCA = RuntimeManager.GetVCA("VCA:/Master");
+    }
+
+    private void Update()
+    {
+        float value;
+        masterVCA.getVolume(out value);
+        volumeSlider.value = value;
     }
 
     public void ChangeVolume(float value)
